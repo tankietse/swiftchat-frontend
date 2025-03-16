6d06baeea0c1c5d751fd1bea587063341b2e01ff
tankietse <kiettuanleo@gmail.com>
2025-03-16 23:11:23 +0700

    feat: add chat UI images and update layout metadata with Toaster component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.unsplash.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
